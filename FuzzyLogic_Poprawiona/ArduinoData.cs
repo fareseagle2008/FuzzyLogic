58dfcb28bb5a59b58499770770764b72a7688f50
R0ut <kkaacczzoorr@gmail.com>
2016-12-10 21:44:13 +0100

    Include arduino class
    In arduino class, physical device(arduino) connect by COM port.  Main program change pictureBox day to night when sensor of light have little value.

---
+++ FuzzyLogic_Poprawiona/ArduinoData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace FuzzyLogic_Poprawiona
+{
+    class ArduinoData
+    {
+        SerialPort myPort;
+        public ArduinoData() {
+            try
+            {
+                myPort = new SerialPort();
+                myPort.BaudRate = 9600;
+                myPort.PortName = "COM6"; //pod jakim com jest podlaczone trzeba sprawdzic        
+                myPort.Open();
+                myPort.DtrEnable = true;
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Connection problem");
+            }
+        }
+
+        public int Photoresistor()
+        {
+            int data = Convert.ToInt16(myPort.ReadLine());
+
+            return data;
+        }
+
+
+}
+}
--- FuzzyLogic_Poprawiona/Form1.cs
+++ FuzzyLogic_Poprawiona/Form1.cs
@@ -5,31 +5,37 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+
+
 using System.Windows.Forms;
 
 namespace FuzzyLogic_Poprawiona
 {
     public partial class Form1 : Form
-    {
+    { 
+        ArduinoData arduino;
+        Thread t;
         
         public Form1()
         {
             InitializeComponent();
-            timerTemperatury.Start();
+            arduino = new ArduinoData();
+           
+            
             labelTempWnetrze.Text = Randomizer.GetRandomNumber(5, 30).ToString("#.00");
             labelTempDwor.Text = Randomizer.GetRandomNumber(-20, 40).ToString("#.00");
             
+            
         }
 
-
        
+
+        
+
         private void timerTemperatury_Tick(object sender, EventArgs e)
         {
-            
-            ZmianaPoryDnia(czas);
-            ++czas;
-            if (czas == 37) czas = 0;
+             
             Logika logika = new Logika(Convert.ToDouble(labelTempWnetrze.Text), Convert.ToDouble(labelTempDwor.Text));
             logika.Reguly();
             logika.OdswiezImg(pictureBoxKlima, pictureBoxPiecyk,pictureBox1,labelTempWnetrze);
@@ -37,10 +43,11 @@ namespace FuzzyLogic_Poprawiona
             mocPiec.Text = logika.pc.ToString();
             mocKlima.Text = logika.kl.ToString();
             mocSciana.Text = logika.wp.ToString();
-            
-            
-        }
+            label1.Text = arduino.Photoresistor().ToString();
+            ZmianaPoryDnia(arduino.Photoresistor());
 
+        }
+        
 
         public static bool oknoOtwarte = false;
         private void pictureBoxOkno_Click(object sender, EventArgs e)
@@ -59,48 +66,63 @@ namespace FuzzyLogic_Poprawiona
             }
         }
 
-        int czas = 0;
+        
         private void ZmianaPoryDnia(int czas)
         {
 
+             timerTemperatury.Stop();
             
-            if (czas <= 20)
-             {
-               pictureBoxPoraDnia.Image = Properties.Resources.dzien;
-               pictureBoxPoraDnia.Invalidate();
-             }
-            else
-            {
-                pictureBoxPoraDnia.Image = Properties.Resources.noc;
-                pictureBoxPoraDnia.Invalidate();
-            }
+            
+                if (czas <= 500)
+                {
+                    pictureBoxPoraDnia.Image = Properties.Resources.dzien;
+                    pictureBoxPoraDnia.Invalidate();
+                }
+                else
+                {
+                    pictureBoxPoraDnia.Image = Properties.Resources.noc;
+                    pictureBoxPoraDnia.Invalidate();
+                }
+            
+           timerTemperatury.Start();
         }
 
         private void buttonNewRandom_Click(object sender, EventArgs e)
         {
+            bool poprawne = true;
+
             FormaRandomizujaca forma = new FormaRandomizujaca();
             forma.ShowDialog();
+            
             if(forma.DialogResult == DialogResult.OK)
             {
-                try
+                do
                 {
-                    forma.NowyRandom(labelTempWnetrze, labelTempDwor);
-                }
-                catch(Exception ex)
-                {
-                    if(forma.minRandomek.Text == "")
-                       MessageBox.Show("Pole temperatura minimalna jest puste");
-                    else if (forma.maxRandomek.Text == "")
-                       MessageBox.Show("Pole temperatura maksymalna jest puste");
-                    else
-                        MessageBox.Show("Error");
-                    forma.ShowDialog();
+                    try
+                    {
+                        forma.NowyRandom(labelTempWnetrze, labelTempDwor);
+                        poprawne = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        poprawne = false;
 
-                }
+                        if (forma.minRandomek.Text == "")
+                            MessageBox.Show("Pole temperatura minimalna jest puste");
+                        else if (forma.maxRandomek.Text == "")
+                            MessageBox.Show("Pole temperatura maksymalna jest puste");
+                        else
+                            MessageBox.Show("Error");
+                        forma.ShowDialog();
+
+                    }
+                } while (poprawne != true);
             }
             
         }
 
-        
+       
+
+      
     }
 }
